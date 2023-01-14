dedaf28908173629d698816e0f47635db44da53b
keneNwobodo <nwobodokenechukwu2@gmail.com>
2023-01-14 19:55:03 +0100

    Consumed the Open Weather API and got the current temperature readings of cities and countries. Made the app responsive across devices.

---
+++ key.js
@@ -0,0 +1,2 @@
+// export API key
+API_KEY = "eff96c23553a979b9e7a25ca095d8a95";
--- main.js
+++ main.js
@@ -1,2 +1,47 @@
 const result = document.querySelector(".result");
 const searchBtn = document.querySelector(".search-btn");
+const cityRef = document.querySelector(".searchInput");
+
+function ShowWeather() {
+  let cityName = cityRef.value;
+  // validate if input is empty
+  if (cityName.length == 0) {
+    result.innerHTML = `<h3 class="error">The Input Field cannot be empty</h3>`;
+  } else {
+    let APIURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
+    result.innerHTML = `<h3 class="loading">The city info is laoding...</h3>`;
+
+    // Clear the input value
+    cityRef.value = "";
+    fetch(APIURL)
+      .then((res) => res.json())
+      .then((data) => {
+        result.innerHTML = `
+        <h2>${data.name}</h2>
+        <h4 class="weather"> ${data.weather[0].main}</h4>
+        <h4 class="description"> ${data.weather[0].description}</h4>
+        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" />
+        <h1>${data.main.temp} &#176;</h1>
+        <div class="temp-container">
+         <div>
+          <h4 class="title">Min</h4>
+          <h4 class="temp">${data.main.temp_min}</h4>
+         </div>
+         <div>
+          <h4 class="title">Max</h4>
+          <h4 class="temp">${data.main.temp_max}</h4>
+         </div>
+        </div>
+         
+         `;
+      })
+
+      // Error if API call uncessful
+      .catch((err) => {
+        result.innerHTML = `<h3 class="error">City Not Found</h3>`;
+      });
+  }
+}
+
+// Load event
+searchBtn.addEventListener("click", ShowWeather);
